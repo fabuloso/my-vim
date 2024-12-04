f0defb1e087fae9389f9807850e00ffceaac15f3
Alessandro <dalfovo.alessandro@gmail.com>
2024-12-04 15:03:37 +0100

    Set up commands for rust

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,1 +1,5 @@
-vim.keymap.set('n', '<space>ca', '<Plug>RustHoverAction')
+vim.keymap.set('n', '<space>ca', '<Cmd>RustLsp codeAction')
+vim.keymap.set('v', '<space>ca', '<Cmd>RustLsp rangeCodeAction')
+vim.keymap.set('n', '<space>cr', '<Cmd>RustLsp rename<CR>')
+vim.keymap.set('n', '<space>ce', '<Cmd>RustLsp explainError<CR>')
+vim.keymap.set('n', '<space>cm', '<Cmd>RustLsp expandMacro<CR>')
