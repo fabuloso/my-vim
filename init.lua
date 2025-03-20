0d9fc16a395c9be38a357ac6ed8a7d0eda885bd5
Alessandro <dalfovo.alessandro@gmail.com>
2025-03-20 15:24:15 +0100

    Fix installation of lazy

--- init.lua
+++ init.lua
@@ -1,39 +1,39 @@
-vim.g.mapleader = ' '
-local lazy = {}
-
-function lazy.install(path)
-	if not vim.loop.fs_stat(path) then
-		print('Installing lazy.nvim....')
-		vim.fn.system({
-			'git',
-			'clone',
-			'--filter=blob:none',
-			'https://github.com/folke.lazy.nvim.git',
-			'--branch=stable',
-			path,
-		})
-	end
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
+vim.opt.rtp:prepend(lazypath)
 
-function lazy.setup(plugins)
-	lazy.install(lazy.path)
-	vim.opt.rtp:prepend(lazy.path)
-	require('lazy').setup(plugins, lazy.opts)
-end
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-vim.opt.termguicolors = true
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = 'plugins',
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+
 vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<cr>', { silent = true })
 
-lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-lazy.opts = {
-	dev = {
-		path = "~/lazy",
-		patterns = {},
-		fallback = false,
-	},
-}
-
-lazy.setup("plugins")
 
 vim.cmd.colorscheme "catppuccin-mocha"
 vim.opt.number = true
